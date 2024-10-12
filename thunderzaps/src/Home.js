7a73ff27cda311fc011645c243cea9bf7279cd12
lynnlo <tharlynnhtet.ong@gmail.com>
2024-10-12 21:34:24 +0000

    Tab system

---
+++ thunderzaps/src/Home.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const style = {
+    home: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#60a060",
+    },
+};
+
+export const Home = () => {
+    return (
+        <div style={style.home}>
+            <h1> Hello </h1>
+        </div>
+    );
+};
