f4354fd06dab968c1c2a299dd4db509c3ae4f10e
lynnlo <tharlynnhtet.ong@gmail.com>
2024-10-12 20:41:33 +0000

    Tab System

--- thunderzaps/src/App.js
+++ thunderzaps/src/App.js
@@ -1,11 +1,23 @@
 import { useState } from "react";
 
+const style = {
+    home: {
+        width: "100vw",
+        height: "100vh",
+        margin: 0,
+        backgroundColor: "#a0ffa0",
+    },
+};
+
 export const App = () => {
-    const [message, setMessage] = useState();
-    
-    return (<div>
-    <h1> Test </h1>
-        <button> donate $10 </button>
-        <p> {message} </p>
-    </div>);
+    const [tab, setTab] = useState();
+
+    return (
+        <div style={style.home}>
+            <div>
+                <p> Select Tab </p>
+            </div>
+            <div></div>
+        </div>
+    );
 };
